c37e8ff80b38bb32870db28981a458179b1958e4
Ramil <zarram89@gmail.com>
2024-06-18 23:57:50 +0500

    Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// Утилита общего назначения для получения случайного целого из диапазона
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+// Утилита общего назначения для получения случайного числа с заданной точностью из диапапзона
+const getRandomPositiveFloat = (a, b, digits = 1) => {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
+
+  return parseFloat(result.toFixed(digits));
+};
+
+getRandomPositiveInteger(1, 10);
+getRandomPositiveFloat(1, 10);
